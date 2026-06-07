2bc43ef583e9de0f3a411557fdefd497bb707c86
Mohammad Heydari <m_heydari@razi.cc>
2026-06-07 13:42:37 +0330

    pep8

--- extensions/rtl-fixer/__init__.py
+++ extensions/rtl-fixer/__init__.py
@@ -1,11 +1,10 @@
 from flask import Blueprint, render_template
 
 bp = Blueprint('rtl-fixer', __name__)
-bp.label = 'Text Editing'  #type: ignore
-bp.description = 'Fix rtl problems'  #type: ignore
-bp.author = 'Devixa'  #type: ignore
-bp.icon = 'R' #type: ignore
-
+bp.label = 'Text Editing'  # type: ignore
+bp.description = 'Fix rtl problems'  # type: ignore
+bp.author = 'Devixa'  # type: ignore
+bp.icon = 'R'  # type: ignore
 
 
 @bp.route('/')
